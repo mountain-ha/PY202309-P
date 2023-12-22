88df3d0ebee90f10138c1bafffc3a0944168c6b9
mountain-ha <mountain-ha@naver.com>
2023-12-22 13:34:20 +0900

    main 을 new의 코드로 변경

--- Sourses/main.py
+++ Sourses/main.py
@@ -1,77 +1,76 @@
-import pandas as pd
-import itertools
+def make_karnaugh_map(num_of_variables):
+    if num_of_variables == 2:
+        karnaugh_map = [[0, 1], [2, 3]]
+    elif num_of_variables == 3:
+        karnaugh_map = [[0, 1, 3, 2], [4, 5, 7, 6]]
+    elif num_of_variables == 4:
+        karnaugh_map = [[0, 1, 3, 2], [4, 5, 7, 6], [12, 13, 15, 14], [8, 9, 11, 10]]
+    else:
+        print("이 함수는 2부터 4까지의 변수에 대한 카노맵을 지원합니다.")
+        return None
 
-def generate_gray_code(n):
-    """
-    n 비트에 대한 그레이 코드를 생성합니다.
-    """
-    gray_code = []
-    for i in range(2 ** n):
-        gray_code.append((i >> 1) ^ i)
-    return gray_code
+    return karnaugh_map  
 
-def draw_karnaugh_map(variables):
-    """
-    주어진 변수의 개수에 대한 카르노 맵을 그립니다.
-    """
-    n = 2 ** variables
-    gray_code = generate_gray_code(variables)
+def display_karnaugh_map(karnaugh_map):
+    for row in karnaugh_map:
+        print(row)
 
-    # 카르노 맵을 나타내는 DataFrame 생성
-    karnaugh_map = pd.DataFrame(index=range(n), columns=range(variables))
+def mark_boolean_function_positions(karnaugh_map, positions):
+    for position in positions:
+        for i in range(len(karnaugh_map)):
+            if position in karnaugh_map[i]:
+                j = karnaugh_map[i].index(position)
+                karnaugh_map[i][j] = 'X'
+    return karnaugh_map
 
-    # 각 칸에 해당하는 값 적기
-    for i in range(n):
-        binary_value = format(gray_code[i], f'0{variables}b')  # 이진 값에 선행 0을 포함하여 형식화
-        karnaugh_map.iloc[i] = list(map(int, binary_value))
+def optimize_boolean_function(positions, num_of_variables): #일단 sop를 pos로 바꾸는걸 목표로 했음
+    # 입력된 위치를 이진 문자열로 변환
+    binary_strings = [format(pos, '0{}b'.format(num_of_variables)) for pos in positions]
+    
+    # 중복 제거를 위해 set으로 변환 후 리스트로 다시 변환
+    unique_binary_strings = list(set(binary_strings))
+    
+    # 최적화된 부울 함수 (SOP 형태) 생성
+    minimized_terms_sop = unique_binary_strings
+    
+    # SOP 형태의 항들을 POS 형태로 변환: 변환 과정에서 AB, AB'를 A가 아니라 두개의 항으로 표기하는 문제 있음
+    pos_terms = get_pos_terms(minimized_terms_sop, num_of_variables)
+    
+    return minimized_terms_sop, pos_terms
 
+def get_pos_terms(minimized_terms_sop, num_of_variables):
+    # SOP 형태의 항들을 POS 형태로 변환
+    pos_terms = []
+    for sop_term in minimized_terms_sop:
+        pos_term = ''
+        for i, bit in enumerate(sop_term):
+            if bit == '0':
+                pos_term += f"{chr(ord('A') + i)}'"
+            elif bit == '1':
+                pos_term += f"{chr(ord('A') + i)}"
+        pos_terms.append(pos_term)
+    return pos_terms
+
+
+num_of_variables = int(input("2부터 4까지의 변수의 개수를 입력해주세요: "))
+karnaugh_map = make_karnaugh_map(num_of_variables)
+
+if karnaugh_map:
     print("카르노 맵:")
-    print(karnaugh_map)
+    display_karnaugh_map(karnaugh_map)
 
-    # 각 칸의 숫자와 이진수를 매핑하는 딕셔너리 생성
-    mapping_dict = {}
-    for i in range(n):
-        binary_value = ''.join(map(str, karnaugh_map.iloc[i].tolist()))
-        mapping_dict[i] = binary_value
+    positions = list(map(int, input("부울 함수의 위치를 입력하세요 (공백으로 구분된 정수): ").split()))
+    mark_boolean_function_positions(karnaugh_map, positions)
 
-    print("\n카르노 맵 숫자와 이진수 매핑:")
-    print(mapping_dict)
+    print("부울 함수 표시된 카르노 맵:")
+    display_karnaugh_map(karnaugh_map)
 
-    return karnaugh_map, mapping_dict
+    # 부울 함수 최적화
+    minimized_terms_sop, pos_terms = optimize_boolean_function(positions, num_of_variables)
+    
+    print("최적화된 부울 함수 (SOP):", minimized_terms_sop)
+    print("최적화된 부울 함수 (POS):", pos_terms)
 
-def ope_boolean_functiontimiz(karnaugh_map, mapping_dict, boolean_function):
-    """
-    카르노 맵을 이용하여 부울 함수를 최적화합니다.
-    """
-    num_rows, num_columns = karnaugh_map.shape
-    optimized_terms = []
-
-    for i in range(num_rows):
-        value = karnaugh_map.iloc[i].tolist()
-        if value.count(1) == 1 and boolean_function[i] == '1':
-            term = mapping_dict[i]
-            optimized_terms.append(term)
-
-    if not optimized_terms:
-        return "최적화된 부울 함수가 없습니다."
-
-    optimized_function = " + ".join(optimized_terms)
-    return f"최적화된 부울 함수: {optimized_function}"
-
-# 사용자로부터 변수의 개수를 입력으로 받기
-num_variables = int(input("변수의 개수를 입력하세요 (2에서 4까지): "))
-
-if num_variables < 2 or num_variables > 4:
-    print("변수의 개수는 2에서 4까지의 값이어야 합니다.")
-else:
-    karnaugh_map, mapping_dict = draw_karnaugh_map(num_variables)
-
-    # 사용자로부터 부울 함수 입력 받기 (예: x1'x2 + x1x2')
-    boolean_function = input("부울 함수를 입력하세요: ")
-
-    # 부울 함수를 최적화하여 출력
-    result = optimize_boolean_function(karnaugh_map, mapping_dict, boolean_function)
-    print(result)
 
 
 
