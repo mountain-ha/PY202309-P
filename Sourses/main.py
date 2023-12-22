be3fc1858999d7ff8d4df9477392042088860906
mountain-ha <mountain-ha@naver.com>
2023-12-23 01:31:00 +0900

    수정해봄: 진척 없음

--- Sourses/main.py
+++ Sourses/main.py
@@ -17,41 +17,47 @@ def display_karnaugh_map(karnaugh_map):
 
 def mark_boolean_function_positions(karnaugh_map, positions):
     for position in positions:
-        for i in range(len(karnaugh_map)):
-            if position in karnaugh_map[i]:
-                j = karnaugh_map[i].index(position)
+        for i, row in enumerate(karnaugh_map):
+            if position in row:
+                j = row.index(position)
                 karnaugh_map[i][j] = 'X'
     return karnaugh_map
 
-def optimize_boolean_function(positions, num_of_variables): #일단 sop를 pos로 바꾸는걸 목표로 했음
+def optimize_boolean_function(positions, num_of_variables):
     # 입력된 위치를 이진 문자열로 변환
     binary_strings = [format(pos, '0{}b'.format(num_of_variables)) for pos in positions]
-    
-    # 중복 제거를 위해 set으로 변환 후 리스트로 다시 변환
-    unique_binary_strings = list(set(binary_strings))
-    
+
     # 최적화된 부울 함수 (SOP 형태) 생성
-    minimized_terms_sop = unique_binary_strings
-    
-    # SOP 형태의 항들을 POS 형태로 변환: 변환 과정에서 AB, AB'를 A가 아니라 두개의 항으로 표기하는 문제 있음
-    pos_terms = get_pos_terms(minimized_terms_sop, num_of_variables)
-    
+    minimized_terms_sop = list(set(binary_strings))
+
+    # SOP 형태의 항들을 POS 형태로 변환
+    pos_terms = get_pos_terms(minimized_terms_sop)
+
     return minimized_terms_sop, pos_terms
 
-def get_pos_terms(minimized_terms_sop, num_of_variables):
+def get_pos_terms(minimized_terms_sop):
     # SOP 형태의 항들을 POS 형태로 변환
-    pos_terms = []
-    for sop_term in minimized_terms_sop:
-        pos_term = ''
-        for i, bit in enumerate(sop_term):
-            if bit == '0':
-                pos_term += f"{chr(ord('A') + i)}'"
-            elif bit == '1':
-                pos_term += f"{chr(ord('A') + i)}"
-        pos_terms.append(pos_term)
+    pos_terms = [''.join([f"{chr(ord('A') + i)}'" if bit == '0' else f"{chr(ord('A') + i)}" for i, bit in enumerate(term)]) for term in minimized_terms_sop]
     return pos_terms
 
+def simplify_pos_terms(pos_terms):
+    grouped_terms = {}
+    for term in pos_terms:
+        key = ''.join(sorted(term))
+        grouped_terms.setdefault(key, []).append(term)
 
+    simplified_terms = []
+    for key, terms in grouped_terms.items():
+        if len(terms) == 1:
+            simplified_terms.append(terms[0])
+        else:
+            common_factors = ''.join([c for c in key if key.count(c) == len(terms)])
+            additional_factors = ''.join([c for c in key if key.count(c) < len(terms)])
+            simplified_terms.append(f"({common_factors}+{additional_factors}')")
+
+    return simplified_terms
+
+# 메인 코드
 num_of_variables = int(input("2부터 4까지의 변수의 개수를 입력해주세요: "))
 karnaugh_map = make_karnaugh_map(num_of_variables)
 
@@ -69,7 +75,7 @@ if karnaugh_map:
     minimized_terms_sop, pos_terms = optimize_boolean_function(positions, num_of_variables)
     
     print("최적화된 부울 함수 (SOP):", minimized_terms_sop)
-    print("최적화된 부울 함수 (POS):", pos_terms)
+    print("최적화된 부울 함수 (POS):", get_pos_terms(minimized_terms_sop))
 
 
 
